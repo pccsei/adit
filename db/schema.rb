d1f0f046c3328d16f77094edb3131fa031b4ad0c
noahpc <noahpconrad@gmail.com>
2013-11-08 23:37:22 -0600

    Added the action table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131109053406) do
+ActiveRecord::Schema.define(version: 20131109053628) do
 
   create_table "action_types", force: true do |t|
     t.string   "name"
     t.integer  "role"
     t.integer  "point_value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "actions", force: true do |t|
+    t.integer  "points_earned"
+    t.datetime "user_action_time"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
