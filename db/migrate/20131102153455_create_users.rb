04e91d67cbdba7a297a683f3adb32505e57829f7
noahpc <noahpconrad@gmail.com>
2013-11-09 20:27:01 -0600

    Trying to fix the mess

--- db/migrate/20131102153455_create_users.rb
+++ db/migrate/20131102153455_create_users.rb
@@ -1,17 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.integer :id
       t.string :name
-      t.string :school_id
-      t.integer :role
-      t.integer :section
-      t.string :parent_id
-      t.string :email
-      t.string :mobile_number
-      t.integer :box_number
-      t.string :room_number
-      t.integer :power_token
 
       t.timestamps
     end
