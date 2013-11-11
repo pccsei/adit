3f9f47150b73996ef15bb2e8105397476a515e71
canipejak <jcani@bellsouth.net>
2013-11-11 15:31:15 -0600

    Running test code

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "User cannot be empty" do
+    user = User.new
+    assert user.invalid?
+    assert user.errors[:name].any?
+    assert user.errors[:school_id].any?
+    assert user.errors[:email].any?
+    assert user.errors[:phone].any?
+  end
 end
