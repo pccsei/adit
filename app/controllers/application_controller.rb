35968bc480e12f72b2ca987966dc286e303c0242
noahpc <noahpconrad@gmail.com>
2013-11-26 21:00:53 -0600

    Reworked Current Project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
                helper_method :get_current_project
   
-  def get_current_project
-    project = Project.find_by "project_end > ?", Time.now
+  def are_tickets_open
+    project = Project.find_by "project_end > ? && project_start < ? ", Time.now, Time.now
     return project
   end
+  
+  def get_current_project
+    project = Project.find_by is_current_project: '1'
+    return project
+  end
+  
 end
