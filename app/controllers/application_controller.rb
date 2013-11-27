9acf940690a71fb5dcdb8bd53d3e9ae38831c559
noahpc <noahpconrad@gmail.com>
2013-11-26 21:42:49 -0600

    Added seed data and fixed current project stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-               helper_method :get_current_project
-  
-  def are_tickets_open
-    project = Project.find_by "project_end > ? && project_start < ? ", Time.now, Time.now
-    return project
-  end
-  
+               helper_method :get_current_project # helper_method allow methods to be used in views
+               helper_method :are_tickets_open  
   def get_current_project
     project = Project.find_by is_current_project: '1'
     return project
--- lib/common_methods.rb
+++
@@ -1,6 +0,0 @@
-module CommonMethods
-  def get_current_project
-    project = Project.find_by "project_end > ?", Time.now
-    return project.id
-  end
-end
