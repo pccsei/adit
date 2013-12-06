3791d610d361f29723117ed3ae6ead57ad61b208
noahpc <noahpconrad@gmail.com>
2013-12-05 22:27:00 -0600

    seed file change

--- db/seeds.rb
+++ db/seeds.rb
@@ -499,13 +499,5 @@ clients = Client.create([{business_name: '10th Avenue Hair Designs', address: '1
    #projects = Project.create(year: 2013 , semester: 'Spring', project_type_id: 1, 
     #                         project_start: '25-FEB-2013', project_end: '15-APR-2013', comment: 'This is the true Spring 2013 project',
      #                        max_clients: '5', max_green_clients: '1', max_white_clients: '1', max_yellow_clients: '1', use_max_clients: true)
- #clients = Client.create([{business_name: '10th Avenue Hair Designs', address: '1000 East Cervantes Street', email: '', website: '',  telephone: '433-5207', comment: ''},
-#{business_name: '32 Degrees Yogurt Bar', address: '5046 Bayou Boulevard', email: '', website: '',  telephone: '471-2000', comment: ''},
-#{business_name: '6 Flags Cleaners Laundry', address: '916 W. Michigan Ave., Suite A', email: '', website: '',  telephone: '456-6888', comment: ''},
-#{business_name: '9th Avenue Coin Laundry', address: '6220 North 9th Avenue', email: '', website: '', telephone: '471-9224', comment: ''},
-#{business_name: 'A Backcare Center', address: '5559 North Davis Highway', email: '', website: '', telephone: '475-2675', comment: ''},
-#{business_name: 'A Flower Shop', address: '3709 Mobile Highway', email: '', website: '', telephone: '438-1609', comment: ''},
-#{business_name: 'Able Auto Repair', address: '6548 Mobile Hwy', email: '', website: '',  telephone: '332-5162', comment: ''},
-#{business_name: 'Accents by KellyCo Flowers', address: '615 Scenic Highway', email: '', website: '',  telephone: '438-1981', comment: ''}])
 
    #statuses = Status.create([{status_type: "Not Approved" }, { status_type: "Approved"}, { status_type: "In House" }, { status_type: "Pending" }])
