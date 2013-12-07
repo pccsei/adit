2815a9a12b642dc09885cc68897bbb74dc63aea6
noahpc <noahpconrad@gmail.com>
2013-12-06 19:34:40 -0600

    global variable for current project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :set_selected_project
+
   include SessionsHelper
                helper_method :get_current_project # helper_method allow methods to be used in views
                helper_method :are_tickets_open  
                helper_method :get_selected_project
                helper_method :set_selected_project
-    
+               
+  $selected_project = Project.last
             
   def get_current_project
     project = Project.find_by is_active: '1'
@@ -16,14 +17,12 @@ class ApplicationController < ActionController::Base
   end
   
   def get_selected_project
-    return @selected_project
+    return $selected_project
   end
   
-  def set_selected_project(project = (Project.find_by is_active: '1'))
-    if(!project)
-      project = Project.first
-    end
-    @selected_project = project
+  def set_selected_project(project = Project.last)
+    $selected_project = project
+    return 
   end
   
   
