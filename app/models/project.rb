3849fd59afb5484774db7c84388bf84384263981
noahpc <noahpconrad@gmail.com>
2014-02-17 09:24:35 -0600

    Finished project submit/update form logic

--- app/models/project.rb
+++ app/models/project.rb
@@ -18,7 +18,6 @@ class Project < ActiveRecord::Base
   
 # Validates the max total clients option  
   validates :max_clients, length: {
-<<<<<<< HEAD
     minimum: 1,
     message: 'is the wrong length.  Needs to be one digit long.'
   }, numericality: { greater_than: 0 }, unless: Proc.new { |project| project.use_max_clients == false }
@@ -42,19 +41,7 @@ class Project < ActiveRecord::Base
     minimum: 1,
     message: 'is the wrong length.  Needs to be at least one digit long.'
   }, numericality: { greater_than_or_equal_to: 1, :if => lambda { |project| (project.max_high_priority_clients == 0 && project.max_medium_priority_clients == 0) } }, 
-      unless: Proc.new { |project| project.use_max_clients == true }
-=======
-    minimum: 0, maximum: 1,
-    message: 'is the wrong length.  Needs to be one digit long.'
-  }, numericality: { greater_than: 0 }, unless: Proc.new { |project| project.use_max_clients == false }
-  
-# Validates the max seperate clients option
-  validates :max_high_priority_clients, :max_medium_priority_clients, :max_low_priority_clients, length: {
-    minimum: 0, maximum: 1,
-    message: 'is the wrong length.  Needs to be one digit long.'
-  }, numericality: { greater_than_or_equal_to: 0 }, unless: Proc.new { |project| project.use_max_clients == true }
->>>>>>> Empty
-  
+      unless: Proc.new { |project| project.use_max_clients == true }  
 # Custom method to make sure the start date is before the end date  
   def start_before_end
     if(tickets_open_time && tickets_close_time)
