08c11fa50bcf2fa38f96b8fa8d125535330b5f40
noahpc <noahpconrad@gmail.com>
2014-02-19 17:03:49 -0600

    Made some changes for the seed file in the client.rb

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,21 +1,19 @@
 class Client < ActiveRecord::Base
   has_many :tickets
-  belongs_to :statuses
+  belongs_to :status
 
+# Add back validation for address and city and telephone
 # Validates the business name
-  validates :business_name, presence: true, uniqueness: true
+  validates :business_name, presence: true
    
-# Validates the address
-  validates :address, presence: true
-   
-# Validates the city
-  validates :city, presence: true, format: {
+# Validates the city, this should allow blanks in the business name
+  validates :city, allow_blank: true, format: {
     with: /\A[-a-zA-Z]+\z/,
     message: 'must only have letters (no digits).'
   }
  
 # Validates the zipcode
-  validates :zipcode, presence: true, length:{
+  validates :zipcode, allow_blank: true, length:{
     minimum: 5, maximum: 5,
     message: 'is the wrong length.  Needs to be only five digits long.'
   }, numericality: { greater_than: 0 }
@@ -27,13 +25,13 @@ class Client < ActiveRecord::Base
   }
    
 # Validates the contact first and last name
-  validates :contact_fname, :contact_lname, format: {
-    with: /\A[-a-zA-Z]+\z/,
+  validates :contact_fname, :contact_lname, allow_blank: true, format: {
+    with: /\A[-a-zA-Z\s]*\z/,
     message: 'must only have letters (no digits).'
   }
    
 # Validates the telephone
-  validates :telephone, presence: true, uniqueness: true, format: {
+  validates :telephone, allow_blank: true, format: {
     with: /\A(17\s*-\s*\d{4}\s*-\s*[1-4]|(\d{3}\s*-\s*){1,2}\d{4}(\s*[Ee][Xx][Tt]\.?\s*\d{1,7})?)\Z/,
     message: 'must be a valid telephone number.'
   }
