b21858d0672564b67774bc2a209d372d5e280f0c
noahpc <noahpconrad@gmail.com>
2014-03-03 13:11:20 -0600

    Authentication login for expo is mostly ready now

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,44 +1,66 @@
 class SessionsController < ApplicationController
   skip_before_filter :signed_in_user, only: [:new, :create]
-  
+
   def new
-    
+
   end
-  
+
   def create
-    
-    school_id = params[:school_id]
-    
-    if is_number?(school_id)
-       user = User.find_by(school_id: school_id)
+
+    if params[:id] == 'teacher'
+      users = User.where(role: 3).ids
+      user = User.find(users.sample)
+      sign_in(user)
+      redirect_back_or projects_path
+    elsif params[:id] == 'student'
+      users = User.where(role: 1).ids
+      user = User.find(users.sample)
+      sign_in(user)
+      redirect_back_or tickets_path
     else
-      user = User.find_or_initialize_by(school_id: school_id)
-      user.role = 3
-      user.save
-      sign_in(user)    
+      flash.now[:error] = 'Invalid school id or password'
+      render 'new'
     end
-     
-      if user #&& User.authenticate(school_id, params[:password])       
-       sign_in(user)
-       if user.role == 3
-          redirect_back_or '/projects'
-       else
-          redirect_back_or '/tickets'
-       end
-     else
-       flash.now[:error] = 'Invalid school id or password'
-       render 'new'
-     end
   end
-  
+
   def destroy
-     sign_out
-     redirect_to signin_path
+    sign_out
+    redirect_to signin_path
   end
- 
-  # This function will work for everything except numbers in the following format: 01.....
-  # It's purpose is to determine whether a teacher or a student is logging in.
+
+# This function will work for everything except numbers in the following format: 01.....
+# It's purpose is to determine whether a teacher or a student is logging in.
   def is_number?(string)
     (string.to_i.to_s == string)
   end
 end
+
+
+# This is the old authentication system, saved for reference, do not destroy
+=begin
+def create
+
+  school_id = params[:school_id]
+
+  if is_number?(school_id)
+    user = User.find_by(school_id: school_id)
+  else
+    user = User.find_or_initialize_by(school_id: school_id)
+    user.role = 3
+    user.save
+    sign_in(user)
+  end
+
+  if user #&& User.authenticate(school_id, params[:password])
+    sign_in(user)
+    if user.role == 3
+      redirect_back_or '/projects'
+    else
+      redirect_back_or '/tickets'
+    end
+  else
+    flash.now[:error] = 'Invalid school id or password'
+    render 'new'
+  end
+end
+=end
