d2dc18c8746dd72a217c5e373e4e421d527b8c2c
Stephen Paul Chappell <Noctis.Skytower@gmail.com>
2014-03-08 20:37:33 -0600

    Fixed user and client phone regex

--- app/models/client.rb
+++ app/models/client.rb
@@ -33,7 +33,7 @@ class Client < ActiveRecord::Base
    
 # Validates the telephone
   validates :telephone, allow_blank: true, format: {
-    with: /\A(17\s*-\s*\d{4}\s*-\s*[1-4]|((\d{3}\s*-\s*){1,2}\d{4})?(\s*[Ee][Xx][Tt]\.?\s*\d{1,7})?)\Z/,
+    with: /\A(17\s*-\s*\d{4}\s*-\s*[1-4]|(\d{3}\s*-\s*){1,2}\d{4}(\s*[Ee][Xx][Tt]\.?\s*\d{1,7})?)\Z/,
     message: 'must be a valid telephone number.'
   }
 
