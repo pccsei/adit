6b47eb7e7dc1883111da55ccb18dd6f348668cc5
James Miyashita <tShoemaker@pcci.edu>
2014-03-15 11:50:20 -0500

    button now responds to release comment

--- app/assets/javascripts/controller/receipts.js
+++ app/assets/javascripts/controller/receipts.js
@@ -8,4 +8,11 @@ onLoad(function() {
   $('.closeModal').click(function(){
   	$('.modal').modal('hide');
   });  
+  
+  $("#release_comment").keyup(function(){
+    if ($('#release_comment').val().length > 0)
+      $('#switchText').attr('value', "Comment and Release");
+    else
+      $('#switchText').attr('value', "Release");
+  });
 });
