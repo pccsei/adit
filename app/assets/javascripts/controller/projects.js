8185ad9d42eab29d489c7d8323d059ddfb444653
Stephen Paul Chappell <Noctis.Skytower@gmail.com>
2014-03-22 12:18:55 -0500

    Priority selector counts on project now make sense

--- app/assets/javascripts/controller/projects.js
+++ app/assets/javascripts/controller/projects.js
@@ -14,5 +14,19 @@ onLoad(function() {
             "project[tickets_close_time]": "Please select a close time."
         }
     });
-
+    $('#project_max_clients').change(function(s) {
+        var high = $('#project_max_high_priority_clients')[0];
+        var medium = $('#project_max_medium_priority_clients')[0];
+        var low = $('#project_max_low_priority_clients')[0];
+        high.selectedIndex = Math.min(high.selectedIndex, s.target.selectedIndex);
+        medium.selectedIndex = Math.min(medium.selectedIndex, s.target.selectedIndex);
+        low.selectedIndex = Math.min(low.selectedIndex, s.target.selectedIndex);
+    });
+    function update_project_max_clients(s) {
+        var clients = $('#project_max_clients')[0];
+        clients.selectedIndex = Math.max(clients.selectedIndex, s.target.selectedIndex);
+    }
+    $('#project_max_high_priority_clients').change(update_project_max_clients);
+    $('#project_max_medium_priority_clients').change(update_project_max_clients);
+    $('#project_max_low_priority_clients').change(update_project_max_clients);
 });
