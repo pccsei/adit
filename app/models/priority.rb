fe85d3b53b7bf21a61724592e267a025d61b060e
noahpc <noahpconrad@gmail.com>
2014-03-31 18:46:50 -0500

    Fixed medium/low priority mixup

--- app/models/priority.rb
+++ app/models/priority.rb
@@ -4,19 +4,22 @@ class Priority < ActiveRecord::Base
   #Will be used once new seed file has been created
   def self.retrieve(client, project)
    
-     value = (Priority.find_by name: "medium").id
+     value = (Priority.find_by name: "low").id
      if (project.semester == "Spring")      
-        if (client.receipts.where("made_sale = ? AND ticket_id IN (?)", true,
-           (client.tickets.where("project_id IN (?)", Project.where("year = ?", (project.year - 1)).ids)))).present?
+        if (client.receipts.where("made_sale = ? AND ticket_id IN (?)", 
+                                            true,    (client.tickets.where("project_id IN (?)", 
+                                                                            Project.where("year = ?", (project.year - 1)).ids)))).present?
           value = (Priority.find_by name: "high").id    
-        elsif (client.receipts.where("made_sale = ? AND ticket_id NOT IN (?)", true, (client.tickets.where("project_id IN (?)", Project.where("year < ?", (project.year - 3)).ids)))).present?
-          value = (Priority.find_by name: "low").id
+        elsif (client.receipts.where("made_sale = ? AND ticket_id NOT IN (?)", 
+                                              true, (client.tickets.where("project_id IN (?)", 
+                                                                           Project.where("year < ?", (project.year - 3)).ids)))).present?
+          value = (Priority.find_by name: "medium").id
         end
      else
         if (client.receipts.where("made_sale = ? AND ticket_id IN (?)", true, (client.tickets.where("project_id IN (?)", Project.where("semester = ? AND year = ? OR semester = ? AND year = ?", "Spring", (project.year), "Fall", (project.year - 1)).ids)))).present? 
           value = (Priority.find_by name: "high").id
         elsif (client.receipts.where("made_sale = ? AND ticket_id NOT IN (?)", true, (client.tickets.where("project_id IN (?)", Project.where("semester = ? AND year < ? OR semester = ? AND year < ?", "Spring",(project.year - 2), "Fall", (project.year - 3)).ids)))).present?
-          value = (Priority.find_by name: "low").id
+          value = (Priority.find_by name: "medium").id
         end    
      end
      value
