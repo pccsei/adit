4c4b9e60b8f04a54318ed1cd9a554f1f667ad9c8
noahpc <noahpconrad@gmail.com>
2014-04-05 11:47:20 -0500

    Added better styling to js files.

--- app/assets/javascripts/controller/projects.js
+++ app/assets/javascripts/controller/projects.js
@@ -1,19 +1,17 @@
+//*********************************************************************************************************************/
+// Projects.js - Everything must be wrapped in the onLoad function to handle Turbolinks
+//*********************************************************************************************************************/
 onLoad(function () {
+
+//*********************************************************************************************************************/
+// Projects new/edit  - Everything must be wrapped in the onLoad function to handle Turbolinks
+//*********************************************************************************************************************/
 
     $('#project_tickets_open_time').datetimepicker({ dateFormat: "yy/mm/dd", timeFormat: "hh:mm TT"});
     $('#project_tickets_close_time').datetimepicker({ dateFormat: "yy/mm/dd", timeFormat: "hh:mm TT" });
 
-    $("#new_project").validate({
-        rules: {
-            "project[tickets_open_time]": {required: true, date: true},
-            "project[tickets_close_time]": {required: true, date: true}
 
-        },
-        messages: {
-            "project[tickets_open_time]": "Please select an open time.",
-            "project[tickets_close_time]": "Please select a close time."
-        }
-    });
+    // These three code blocks make the priorities choices logical
     $('#project_max_clients').change(function (s) {
         var high = $('#project_max_high_priority_clients')[0];
         var medium = $('#project_max_medium_priority_clients')[0];
@@ -25,6 +23,7 @@ onLoad(function () {
             low.selectedIndex = Math.min(low.selectedIndex, selected_index);
         }
     });
+
     function update_project_max_clients(s) {
         var clients = $('#project_max_clients')[0];
         var selected_index = clients.selectedIndex;
@@ -32,8 +31,8 @@ onLoad(function () {
             clients.selectedIndex = Math.max(selected_index, s.target.selectedIndex);
         }
     }
-
     $('#project_max_high_priority_clients').change(update_project_max_clients);
+
     $('#project_max_medium_priority_clients').change(update_project_max_clients);
     $('#project_max_low_priority_clients').change(update_project_max_clients);
 });
