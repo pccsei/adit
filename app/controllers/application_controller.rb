2384ecf4ca032580763a3923ceb06981ee87ed4b
noahpc <noahpconrad@gmail.com>
2014-04-12 21:27:17 -0500

    A little more work on current section/project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -51,7 +51,7 @@ class ApplicationController < ActionController::Base
   
   def get_selected_project
     # EXPO rewrite after the expo, added the member stuff
-    member = Member.where(user_id: current_user.id).first
+    member = Member.where(user_id: current_user.id).last
     if member
        project = member.project
     else
@@ -74,14 +74,14 @@ class ApplicationController < ActionController::Base
   def get_selected_section
     if session[:selected_section_id]
        section = session[:selected_section_id]
-    elsif current_user.members.where(project_id: get_selected_project).first.present?
-      section = current_user.members.where(project_id: get_selected_project).first.section_number
-      set_selected_section(section)
+    elsif current_user.members.where(project_id: get_selected_project).present?
+       section = current_user.members.where(project_id: get_selected_project).first.section_number
+       set_selected_section(section)
     else
        section = 'all'
        set_selected_section(section)
     end
-      section
+       section
   end
 
   def get_array_of_all_sections(selected_project)
