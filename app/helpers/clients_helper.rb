d8de99420abfc9b913d897e975f9449407d82040
noahpc <noahpconrad@gmail.com>
2014-04-14 09:23:52 -0500

    Commented all the helpers

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -1,24 +1,13 @@
-module ClientsHelper
-  
-  def ticket_taken(cid) 
-    ticket = Ticket.where(client_id: cid, project_id: get_selected_project.id).first  
-  
-    if ticket
-      if ticket.user_id != 0 && ticket.user_id != nil
-        false
-      else
-        ticket.user_id
-      end
-    else 
-      true
-    end        
-  end
+module ClientsHelper 
 
+  # Checks to see if the current user is permitted to obtain another client
   def more_tickets_allowed
     Ticket.more_clients_allowed(current_user, get_current_project, current_user.role, 'low')
   end
 
+  # Takes a user's id and displays first name, last name, and school id
   def get_submitter_info(submitter_id, br_include = true)
+    # Check if school id should be on a separate line
     if br_include
       b = "<br />" 
     else 
@@ -27,22 +16,24 @@ module ClientsHelper
     
     u = User.find(submitter_id)
     "#{u.first_name} #{u.last_name}#{b}(#{u.school_id})".html_safe
-  end
+  end 
   
-  
+  # Splits up a phone number so that it can be displayed nicely by the 
+  #   number_to_phone rails helper
   def render_phone(phone_number)
-    if !phone_number.blank?  
-     if (phone_number.include? "Ext.") 
-        ext = phone_number.partition("Ext.")
-        number_to_phone(ext[0], extension: ext[2])
-     elsif phone_number.include? "ext."
-       ext = phone_number.partition("ext.")
-       number_to_phone(ext[0], extension: ext[2])
+     if phone_number.present?  
+        if (phone_number.include? "Ext.") 
+           ext = phone_number.partition("Ext.")
+           number_to_phone(ext[0], extension: ext[2])
+        elsif phone_number.include? "ext."
+           ext = phone_number.partition("ext.")
+           number_to_phone(ext[0], extension: ext[2])
+        else
+           number_to_phone(phone_number)
+        end
      else
-       number_to_phone(phone_number)
+        nil
      end
-   else
-     nil
   end
- end
+  
 end
