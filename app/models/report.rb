0575d2bcdcc873a5b354e056834ee188c86c0533
Zach Evans <zevans8222@students.pcci.edu>
2014-04-18 23:06:55 -0500

    styled bonus sections

--- app/models/report.rb
+++ app/models/report.rb
@@ -31,7 +31,7 @@ class Report
       sales[index].payment_type = r.payment_type
       sales[index].section      = Member.get_section_number(r.user_id, project)
       if (Action.find_by(receipt_id: r.id).action_type_id)
-        sales[index].ad_status  = ActionType.find(Action.find_by(receipt_id: r.id, action_type_id: [ActionType.where(name: "Old Sale").id,ActionType.where(name: "New Sale").id]).action_type_id).name
+        sales[index].ad_status  = ActionType.find(Action.find_by(receipt_id: r.id, action_type_id: [ActionType.find_by(name: "Old Sale").id,ActionType.find_by(name: "New Sale").id]).action_type_id).name
       end
       index = index + 1
     end
