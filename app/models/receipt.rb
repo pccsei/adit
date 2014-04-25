cf836fe9518d9867c11be3dea7be31b7d756210d
noahpc <noahpconrad@gmail.com>
2014-04-25 08:37:53 -0500

    Removed references to Bonuses while we recreate them

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -14,10 +14,6 @@ class Receipt < ActiveRecord::Base
 		message: 'can have a maximum of 6 digits. Please turn Javascript back on.'
   }
   
-  # def self.released_receipts
-    # all :conditions => {['']}
-  # end
-  
   # Returns all the receipts for the selected project
   def self.selected_project_receipts(project)
     
@@ -75,7 +71,8 @@ class Receipt < ActiveRecord::Base
       end
     end
     
-    return points + Bonus.get_student_bonus_total(student_id, project.id)
+    #return points + Bonus.get_student_bonus_total(student_id, project.id) BONUS
+    return points
   end
   
   def self.release!
