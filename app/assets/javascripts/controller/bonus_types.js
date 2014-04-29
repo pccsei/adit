d8d74401022ddfa7a932494207bb7695ca75aeb5
canipejak <jcani@bellsouth.net>
2014-04-28 21:43:26 -0500

    added bonus validation and back buttons

--- app/assets/javascripts/controller/bonus_types.js
+++ app/assets/javascripts/controller/bonus_types.js
@@ -12,17 +12,26 @@ onLoad(function() {
         return this.optional(element) || /^(\d{0,3})$/.test(value);
     });
 	
-	$("#student_options").validate({
+	// Custom method to make sure a number does not start with a zero
+    jQuery.validator.addMethod("leading_zero", function(value, element) {
+        return this.optional(element) || /^([1-9]+(\d)*|[-a-zA-Z1-9]+)$/.test(value);
+    });
+    
+	$("#bonus_types").validate({
 		rules: {
-			"bonus_points":  {required:  true, min: 1, max: 100, valid_bonus: true, leading_zero: true},
-			"bonus_comment": {maxlength: 250}
+			"bonus_type[name]":        {required: true, maxlength:   250},
+			"bonus_type[point_value]": {required: true, valid_bonus: true, min: 1, max: 100, leading_zero: true}
 		},
 		messages: {
-			"bonus_points": {
+			"bonus_type[name]": {
+				required:     "Please enter the Bonus Name.",
+				maxlength:    "The maximum length for a name is 250 characters."
+			},	
+			"bonus_type[point_value]": {
 				required:     "Please enter the amount of bonus points.",
+				valid_bonus:  "Can only be a number from 1-100 (no decimal values).",
 				min:          "Cannot be less than 1.",
 				max:          "The maximum amount of bonus points is 100.",
-				valid_bonus:  "Can only be a number from 1-100 (no decimal values).",
 				leading_zero: "Cannot begin with zero."
 			}
 		}
